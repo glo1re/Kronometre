91835eb66bc04791d7338bf793fafa56c8439454
glo1re <146658326+glo1re@users.noreply.github.com>
2024-05-11 22:23:46 +0300

    Add files via upload

---
+++ Kronometre/Kronometre/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dakika=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label1.Text);
+            sayi++;
+            label1.Text = sayi.ToString();
+            if (sayi == 59)
+            {
+                sayi = 0;
+                label1.Text=sayi.ToString();
+                dakika++;
+
+                label2.Text = dakika.ToString();
+            }
+        }
+    }
+}
